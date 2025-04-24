86ea1d5a0a7fd1c258a045ddf39e68c14b131999
Elliot Nunn <elliotnunn@me.com>
2025-04-24 15:25:13 +0800

    Some notes

--- internal/flate/inflate_test.go
+++ internal/flate/inflate_test.go
@@ -9,6 +9,28 @@ import (
 	"testing"
 )
 
+/*
+So what do I actually want to do?
+- Every Read call should be satisfied by creating a memory buffer
+of the desired size plus a bit extra, and extracting that.
+
+- An interesting problem is when we scan far forward: there should be a
+"breadcrumb trail" kept of the block-bit-offset and the dictionary state at that spot.
+
+type resumePoint struct {
+	data []byte // 32kb of dict +/- a healthy amount of data after that point
+	blockByte int64
+	blockBit uint8
+}
+
+from the outside takes a ReaderAt, from the inside is essentially byteReader
+
+// BTW, is there any scope for passthrough? probably not...
+
+func readAtLeast(rp resumePoint, minLen int) ([]byte, resumePoint, error)
+
+*/
+
 func TestFlate(t *testing.T) {
 	f, err := os.Open("/Users/elliotnunn/Documents/mac/primary/macworks sources.txt.zip")
 	if err != nil {
