e8138ea7971e91f55a0a5e383a1bc0a78f623a86
Elliot Nunn <elliotnunn@me.com>
2025-04-25 13:49:31 +0800

    HTTP frontend

--- main.go
+++ main.go
@@ -1,32 +1,13 @@
-/*
-Open problems:
-fork format (the obvious one)
-handling of more ephemeral Finder info, like is-inited and has-custom-icon?
-UTF-8 <-> Macintosh format conversions
-aliases <-> symlinks???
-*/
-
 package main
 
 import (
 	"encoding/hex"
 	"fmt"
 	"io/fs"
+	"net/http"
 	"os"
-	"testing/fstest"
 
 	"github.com/elliotnunn/resourceform/internal/hfs"
-)
-
-type Fmt byte
-
-const (
-	Rez Fmt = iota
-	Darwin
-	AppleDouble
-	AppleSingle
-	HQX
-	MBIN
 )
 
 func dumpFS(fsys fs.FS) {
@@ -64,76 +45,10 @@ func main() {
 	concrete := os.DirFS(base)
 	abstract := Wrapper(concrete)
 
-	// err := fstest.TestFS(abstract, "System 7.0 HD.dsk")
-	// fmt.Println(err)
-
-	// o, err := abstract.Open("nonexistent")
-	// fmt.Println(o, err)
-	// fmt.Println("--------------")
-
-	dumpFS(concrete)
-	fmt.Println("--------------")
-	dumpFS(abstract)
-	fmt.Println("--------------")
-	err := fstest.TestFS(abstract, "System 7.0 HD.dsk")
-	fmt.Println(err)
+	go func() {
+		dumpFS(concrete)
+		fmt.Println("----")
+		dumpFS(abstract)
+	}()
+	http.ListenAndServe(":1993", http.FileServerFS(abstract))
 }
-
-// func main() {
-// 	base := os.Args[1]
-
-// 	disk, err := os.Open(base)
-// 	if err != nil {
-// 		os.Stdout.WriteString(err.Error() + "\n")
-// 		os.Exit(1)
-// 	}
-
-// 	fsys, err := hfs.New(disk)
-// 	if err != nil {
-// 		os.Stdout.WriteString(err.Error() + "\n")
-// 		os.Exit(1)
-// 	}
-// 	dumpFS(fsys)
-
-// 	// fsys2 := os.DirFS("/Users/elliotnunn/mac/resourceform")
-// 	// dumpFS(fsys2)
-
-// 	// f, err := fsys.Open("Macintosh HD/System Folder/System" + ResForkSuffix)
-// 	// if err != nil {
-// 	// 	panic(err)
-// 	// }
-// 	// text, err := io.ReadAll(f)
-// 	// if err != nil {
-// 	// 	panic(err)
-// 	// }
-// 	// fmt.Println(string(deRez(text)))
-
-// 	// var wg sync.WaitGroup
-// 	//
-// 	//	filepath.WalkDir(base, func(path string, d fs.DirEntry, err error) error {
-// 	//		name := filepath.Base(path)
-// 	//
-// 	//		if name[0] == '.' || strings.HasSuffix(name, ".rdump") || strings.HasSuffix(name, ".idump") {
-// 	//			if d.IsDir() {
-// 	//				return fs.SkipDir
-// 	//			} else {
-// 	//				return nil
-// 	//			}
-// 	//		}
-// 	//
-// 	//		if d.IsDir() {
-// 	//			return nil
-// 	//		}
-// 	//
-// 	//		println(path)
-// 	//
-// 	//		wg.Add(1)
-// 	//		go func() {
-// 	//			cvt(path, Darwin, Rez)
-// 	//			wg.Done()
-// 	//		}()
-// 	//		return nil
-// 	//	})
-// 	//
-// 	// wg.Wait()
-// }
