4729036e8dd8404f333dd2718529327805fd2b21
Elliot Nunn <elliotnunn@me.com>
2025-04-27 08:00:47 +0800

    HQX: future work

---
+++ internal/hqx/hqx.go
@@ -0,0 +1,10 @@
+package apm
+
+// import (
+// 	"bufio"
+// 	"io"
+// )
+
+// func NewReader(disk io.ReaderAt) (io.ReaderAt, int64, io.ReaderAt, int64, error) {
+// 	r := bufio.NewReader(io.NewSectionReader(disk, 0, len(buf)))
+// }
