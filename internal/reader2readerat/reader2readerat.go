a09f12b718ea8ce71d24c0df198302c745691b94
Elliot Nunn <elliotnunn@me.com>
2025-05-30 17:34:04 +0800

    reader2reader as a different take

---
+++ internal/reader2readerat/reader2readerat.go
@@ -0,0 +1,109 @@
+package reader2readerat
+
+import (
+	"io"
+	"io/fs"
+)
+
+// If the io.Reader is an io.ReadCloser then it will be closed when I am closed
+func NewFromReader(f func() (io.Reader, error)) *Reader {
+	s := initCommon()
+	s.open = func() error {
+		r, err := f()
+		s.r = r
+		return err
+	}
+	s.close = func() {
+		if closer, ok := s.r.(io.Closer); ok {
+			closer.Close()
+		}
+		s.r = nil
+	}
+	return s
+}
+
+func NewFromReadSeeker(r io.ReadSeeker) *Reader {
+	s := initCommon()
+	s.open = func() error {
+		_, err := r.Seek(0, io.SeekStart)
+		s.r = r
+		return err
+	}
+	s.close = func() {
+		s.r = nil
+	}
+	return s
+}
+
+func initCommon() *Reader {
+	r := &Reader{
+		req: make(chan request),
+		rep: make(chan reply),
+	}
+	go r.goro()
+	return r
+}
+
+type Reader struct {
+	r     io.Reader
+	open  func() error
+	close func()
+	req   chan request
+	rep   chan reply
+}
+
+type request struct {
+	buf    []byte
+	offset int64
+}
+
+type reply struct {
+	n   int
+	err error
+}
+
+func (r *Reader) ReadAt(buf []byte, off int64) (n int, err error) {
+	func() {
+		r := recover()
+		if r != nil {
+			n = 0
+			err = fs.ErrClosed
+		}
+	}()
+	r.req <- request{buf, off}
+	rep := <-r.rep
+	return rep.n, rep.err
+}
+
+func (r *Reader) Close() error {
+	close(r.req)
+	return nil
+}
+
+func (r *Reader) goro() {
+	progress := int64(0)
+	for cmd := range r.req {
+		if progress > cmd.offset || r.r == nil {
+			progress = 0
+			r.close()
+			if err := r.open(); err != nil {
+				r.rep <- reply{0, err}
+				r.close()
+				progress = 0
+				continue // make no further attempt to try to satisfy this one
+			}
+		}
+
+		n64, err := io.CopyN(io.Discard, r.r, cmd.offset-progress)
+		progress += n64
+		if err != nil {
+			r.rep <- reply{0, err}
+			continue
+		}
+
+		n, err := r.r.Read(cmd.buf)
+		progress += int64(n)
+		r.rep <- reply{n, err}
+	}
+	r.close()
+}
---
+++ internal/reader2readerat/reader2readerat_test.go
@@ -0,0 +1,53 @@
+package reader2readerat_test
+
+import (
+	"encoding/hex"
+	"io"
+	"math/rand"
+	"testing"
+
+	"github.com/elliotnunn/resourceform/internal/reader2readerat"
+)
+
+type reader byte
+
+func (r *reader) Read(p []byte) (n int, err error) {
+	switch rand.Intn(3) {
+	case 0:
+		p = p[:len(p)-len(p)/2]
+	case 1:
+		p = nil
+	case 2:
+	}
+
+	for i := range p {
+		p[i] = byte(*r)
+		*r++
+	}
+	return len(p), nil
+}
+
+func TestDecompressionCache(t *testing.T) {
+	ra := reader2readerat.NewFromReader(func() (io.Reader, error) {
+		var newreader reader
+		return &newreader, nil
+	})
+
+	for range 100 {
+		offset := int64(rand.Intn(1000))
+		buf := make([]byte, rand.Intn(1000))
+		n, err := ra.ReadAt(buf, offset)
+		if err != nil {
+			t.Errorf("got error %v", err)
+		}
+		if n < 0 || n > len(buf) {
+			t.Errorf("expected %d-%d bytes, got %d", 0, len(buf), n)
+		}
+		for i, c := range buf[:n] {
+			if c != byte(offset)+byte(i) {
+				t.Errorf("expected to start with byte %02x, got %s", byte(offset), hex.EncodeToString(buf[:n]))
+				break
+			}
+		}
+	}
+}
