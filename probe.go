871565fc1df34cc755eb6d132e91cf15148cdfe6
Elliot Nunn <elliotnunn@me.com>
2025-09-13 17:14:31 +0800

    Remove superfluous SectionReader for tar

--- probe.go
+++ probe.go
@@ -83,7 +83,7 @@ func (fsys *FS) probeArchive(subsys fs.FS, subname string) (fsysGenerator, error
 	case matchAt("rLau", 10) || matchAt("StuffIt (c)1997-", 0):
 		return func(r io.ReaderAt) (fs.FS, error) { return sit.New(r) }, nil
 	case matchAt("ustar\x00\x30\x30", 257), matchAt("ustar\x20\x20\x00", 257): // posix tar
-		return func(r io.ReaderAt) (fs.FS, error) { return tar.New(io.NewSectionReader(r, 0, math.MaxInt64)) }, nil
+		return func(r io.ReaderAt) (fs.FS, error) { return tar.New(r) }, nil
 	case matchAt("BD", 1024):
 		return func(r io.ReaderAt) (fs.FS, error) { return hfs.New(r) }, nil
 	}
