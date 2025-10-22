c5c60a356b500902b24ce6bd691898b185dea83b
Elliot Nunn <elliotnunn@me.com>
2025-10-22 09:53:52 +0800

    Simplify Apple Partition Map exposure

--- internal/apm/apm.go
+++ internal/apm/apm.go
@@ -4,16 +4,21 @@
 package apm
 
 import (
+	"cmp"
 	"encoding/binary"
 	"errors"
-	"fmt"
 	"io"
 	"io/fs"
+	"slices"
+	"strconv"
 	"strings"
+	"time"
+
+	"github.com/elliotnunn/BeHierarchic/internal/fskeleton"
 )
 
 // Apple Partition Map
-func New(disk io.ReaderAt) (*FS, error) {
+func New(disk io.ReaderAt) (fs.FS, error) {
 	var ddm [514]byte
 	n, _ := disk.ReadAt(ddm[:], 0)
 	if n < 514 || ddm[0] != 'E' || ddm[1] != 'R' {
@@ -43,21 +48,30 @@ func New(disk io.ReaderAt) (*FS, error) {
 		return nil, errors.New("truncated Apple Partition Map")
 	}
 
-	fs := &FS{
-		disk:   disk,
-		list:   make([]partition, 0, count),
-		search: make(map[string]*partition, count),
-	}
+	fsys := fskeleton.New()
+	defer fsys.NoMore()
+
+	var entries [][]byte
 	for i := range count {
-		ent := apm[int64(i)*mapEntryStep:]
+		ent := apm[int64(i)*mapEntryStep:][:512]
 		if ent[0] != 'P' || ent[1] != 'M' {
 			return nil, errors.New("corrupt Apple Partition Map")
 		}
+		entries = append(entries, ent[:512])
+	}
+
+	// Put them disk order
+	slices.SortStableFunc(entries, func(a, b []byte) int {
+		return cmp.Compare(binary.BigEndian.Uint32(a[8:]), binary.BigEndian.Uint32(b[8:]))
+	})
+
+	ofeach := make(map[string]int)
+	for _, ent := range entries {
 		pmPyPartStart := binary.BigEndian.Uint32(ent[8:])
 		pmPartBlkCnt := binary.BigEndian.Uint32(ent[12:])
-		pmPartName, _, _ := strings.Cut(string(ent[16:48]), "\x00")
+		// pmPartName, _, _ := strings.Cut(string(ent[16:48]), "\x00") // does not reflect later name changes
 		pmParType, _, _ := strings.Cut(string(ent[48:80]), "\x00")
-		pmProcessor, _, _ := strings.Cut(string(ent[120:136]), "\x00")
+		// pmProcessor, _, _ := strings.Cut(string(ent[120:136]), "\x00")
 		var pmPadCode [4]byte
 		copy(pmPadCode[:], ent[136:])
 
@@ -66,71 +80,15 @@ func New(disk io.ReaderAt) (*FS, error) {
 		}
 
 		name := pmParType // e.g. Apple_HFS, Apple_Driver43
-		if strings.Contains(pmParType, "Driver") {
-			if pmProcessor != "" { // e.g. 68000
-				name += "," + pmProcessor
-			}
-			if code := formatPadCode(pmPadCode); code != "" {
-				name += "," + code
-			}
-		} else {
-			if pmPartName != "" { // e.g. Macintosh HD
-				name += "," + pmPartName
-			}
-		}
+		name = strings.TrimPrefix(name, "Apple_")
+		name = strings.ToLower(name)
+		ofeach[name]++
+		name += "-" + strconv.Itoa(ofeach[name])
 
-		for n := 0; ; n++ {
-			try := name
-			if n > 0 {
-				try = fmt.Sprintf("%s,%d", try, n)
-			}
-			if _, dup := fs.search[try]; !dup {
-				break
-			}
-		}
+		pstart, plen := int64(mapEntryStep)*int64(pmPyPartStart), int64(mapEntryStep)*int64(pmPartBlkCnt)
 
-		fs.list = append(fs.list, partition{
-			name:   name,
-			offset: int64(mapEntryStep) * int64(pmPyPartStart),
-			len:    int64(mapEntryStep) * int64(pmPartBlkCnt)})
-		fs.search[name] = &fs.list[len(fs.list)-1]
+		fsys.CreateRandomAccessFile(name, int64(pmPyPartStart),
+			io.NewSectionReader(disk, pstart, plen), plen, 0, time.Time{}, nil)
 	}
-	return fs, nil
-}
-
-type partition struct {
-	name        string
-	offset, len int64
-}
-
-type FS struct {
-	disk   io.ReaderAt
-	list   []partition
-	search map[string]*partition
-}
-
-func (fsys *FS) Open(name string) (fs.File, error) {
-	if name == "." {
-		return &openRoot{fsys: fsys}, nil
-	} else if part, ok := fsys.search[name]; ok {
-		return &openPart{
-			SectionReader: io.NewSectionReader(fsys.disk, part.offset, part.len),
-			part:          part,
-		}, nil
-	}
-	return nil, fs.ErrNotExist
-}
-
-func formatPadCode(c [4]byte) string {
-	if string(c[:]) == "\x00\x01\x06\x00" {
-		return "SCSI"
-	} else if string(c[:]) == "\x00\x00\x00\x00" {
-		return ""
-	}
-	for _, ch := range c {
-		if (ch < '0' || ch > '9') && (ch < 'a' || ch > 'z') && (ch < 'A' || ch > 'Z') {
-			return fmt.Sprintf("%02x%02x%02x%02x", c[0], c[1], c[2], c[3])
-		}
-	}
-	return string(c[:])
+	return fsys, nil
 }
