35e65b68d03fa7634fb4f815f79285e4aa4e348a
Elliot Nunn <elliotnunn@me.com>
2025-12-19 10:55:53 +0800

    Bring search and listing into the main package

--- main.go
+++ main.go
@@ -6,11 +6,17 @@ package main
 import (
 	"errors"
 	"fmt"
+	"io"
+	"io/fs"
 	"net/http"
+	"net/url"
 	"os"
+	"strings"
+	"time"
 
 	_ "net/http/pprof"
 
+	"github.com/bmatcuk/doublestar/v4"
 	"github.com/elliotnunn/BeHierarchic/internal/webdavfs"
 )
 
@@ -47,7 +53,167 @@ func cmdLine(args []string) error {
 	fsys := Wrapper(os.DirFS(target), os.Getenv("BECACHE"))
 	go fsys.Prefetch()
 
-	http.Handle("/", &webdavfs.Handler{FS: fsys})
-
+	webdav := webdavfs.Handler{FS: fsys}
+	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch {
+		case strings.HasSuffix(r.URL.Path, "/.glob.html"):
+			searchPage(fsys, w, r)
+		case (r.Method == "GET" || r.Method == "HEAD") && strings.HasSuffix(r.URL.Path, "/"):
+			dirPage(fsys, w, r)
+		default:
+			webdav.ServeHTTP(w, r)
+		}
+	}))
 	return http.ListenAndServe(port, nil)
 }
+
+func dirPage(fsys *FS, w http.ResponseWriter, r *http.Request) {
+	pathname := strings.Trim(r.URL.Path, "/")
+	if pathname == "" {
+		pathname = "."
+	}
+
+	f, err := fsys.Open(pathname)
+	if err != nil {
+		http.Error(w, err.Error(), 404)
+		return
+	}
+	d, ok := f.(fs.ReadDirFile)
+	if !ok {
+		http.Error(w, "could not assert fs.ReadDirFile", 404)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, "<!doctype html>\n")
+	fmt.Fprintf(w, "<meta name=\"viewport\" content=\"width=device-width\">\n")
+	fmt.Fprint(w, "<h1>BeHierarchic</h1>")
+	fmt.Fprint(w, "<h2>")
+	breadcrumb(w, pathname)
+	fmt.Fprint(w, "</h2>")
+	fmt.Fprintf(w, `<form action=".glob.html" method="GET">`+
+		`<input type="text" name="q" size="50" placeholder="Pattern e.g. **/*.sit">`+
+		`<button type="submit">Glob Search</button></form>`)
+	fmt.Fprintf(w, "<pre>")
+	for {
+		list, err := d.ReadDir(100)
+		for _, de := range list {
+			slash := ""
+			if de.IsDir() {
+				slash = "/"
+			}
+			fmt.Fprintf(w, `<a href="%s%s%s">%s%s</a>`+"\n",
+				r.URL.Path, urlenc(de.Name()), slash,
+				htmlReplacer.Replace(de.Name()), slash)
+		}
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Println(htmlReplacer.Replace(err.Error()))
+		}
+	}
+}
+
+func rel(child, parent string) string {
+	if parent == child {
+		return "."
+	} else if parent == "." {
+		return child
+	} else if len(child) > len(parent) && child[:len(parent)] == parent && child[len(parent)] == '/' {
+		return child[len(parent)+1:]
+	} else {
+		return ""
+	}
+}
+
+func searchPage(fsys *FS, w http.ResponseWriter, r *http.Request) {
+	searchroot := strings.TrimSuffix(r.URL.Path, "/.glob.html")
+	searchroot = strings.TrimPrefix(searchroot, "/")
+	if searchroot == "" {
+		searchroot = "."
+	}
+
+	pattern := r.URL.Query().Get("q")
+	if !doublestar.ValidatePattern(pattern) {
+		http.Error(w, "not a valid glob pattern", http.StatusNotFound)
+		return
+	}
+
+	o, err := fsys.path(searchroot)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, "<!doctype html>\n")
+	fmt.Fprintf(w, "<meta name=\"viewport\" content=\"width=device-width\">\n")
+	fmt.Fprint(w, "<h1>BeHierarchic Search</h1>")
+	fmt.Fprint(w, "<h2>")
+	breadcrumb(w, searchroot)
+	fmt.Fprint(w, "</h2>")
+	fmt.Fprintf(w, `<form action=".glob.html" method="GET">`+
+		`<input type="text" name="q" value="%s" size="50" placeholder="Pattern e.g. **/*.sit">`+
+		`<button type="submit">Glob Search</button></form>`,
+		pattern)
+	fmt.Fprintf(w, "<pre>")
+
+	n := 0
+	t := time.Now()
+	for o, kind := range o.deepWalk() {
+		pathname := o.String()
+		relpath := rel(pathname, searchroot)
+		if relpath == "" || relpath == "." {
+			continue
+		}
+		matches := doublestar.MatchUnvalidated(pattern, relpath)
+		if !matches && kind.IsDir() {
+			matches = doublestar.MatchUnvalidated(pattern, relpath+"/")
+		}
+		if !matches {
+			continue
+		}
+		if kind.IsDir() {
+			relpath += "/"
+		}
+
+		fmt.Fprintf(w, `<a href="/%s">%s</a>`+"\n",
+			urlenc(pathname),
+			htmlReplacer.Replace(relpath))
+
+		n++
+		if n == 2000 {
+			fmt.Fprintln(w, "Limited results")
+			break
+		}
+	}
+	fmt.Fprintf(w, "%d results in %s", n, time.Since(t))
+}
+
+func breadcrumb(w io.Writer, path string) {
+	fmt.Fprint(w, `<a href="/">/</a>`)
+	if path != "." {
+		steps := strings.Split(path, "/")
+		for i := range steps {
+			url := strings.Join(steps[:i+1], "/")
+			fmt.Fprintf(w, "<a href=\"/%s\">%s</a>/", urlenc(url), htmlReplacer.Replace(steps[i]))
+		}
+	}
+}
+
+func urlenc(s string) string {
+	url := url.URL{Path: s}
+	return url.String()
+}
+
+var htmlReplacer = strings.NewReplacer(
+	"&", "&amp;",
+	"<", "&lt;",
+	">", "&gt;",
+	// "&#34;" is shorter than "&quot;".
+	`"`, "&#34;",
+	// "&#39;" is shorter than "&apos;" and apos was not in HTML until HTML5.
+	"'", "&#39;",
+	"\r", `\r`,
+	"\n", `\n`,
+)
--- path.go
+++ path.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"io"
 	"io/fs"
+	"iter"
 	gopath "path"
 	"slices"
 	"strings"
@@ -52,6 +54,67 @@ func (o path) String() string {
 	}
 	slices.Reverse(warps)
 	return gopath.Join(warps...)
+}
+
+func (o path) deepWalk() iter.Seq2[path, fs.FileMode] {
+	return func(yield func(path, fs.FileMode) bool) {
+		type it struct {
+			next func() (path, fs.FileMode, bool)
+			stop func()
+		}
+		var stack = make([]it, 0, 32)
+		f1, f2 := iter.Pull2(o.flatWalk())
+		stack = append(stack, it{f1, f2})
+
+		defer func() {
+			for _, it := range stack {
+				it.stop()
+			}
+		}()
+
+		for len(stack) != 0 {
+			path, kind, ok := stack[len(stack)-1].next()
+			if !ok {
+				stack = stack[:len(stack)-1]
+				continue
+			}
+			if !yield(path, kind) {
+				return
+			}
+			if kind.IsRegular() {
+				ok, mnt := path.getArchive(false, false)
+				if ok {
+					f1, f2 := iter.Pull2(mnt.flatWalk())
+					stack = append(stack, it{f1, f2})
+				}
+			}
+		}
+	}
+}
+
+func (o path) flatWalk() iter.Seq2[path, fs.FileMode] {
+	return func(yield func(path, fs.FileMode) bool) {
+		if selfWalking, ok := o.fsys.(selfWalking); ok {
+			prefix := o.name.String()
+			for pathname, kind := range selfWalking.Walk(false /*not exhaustive*/) {
+				if rel(pathname, prefix) == "" {
+					continue
+				}
+				ok := yield(path{o.container, o.fsys, internpath.New(pathname)}, kind)
+				if !ok {
+					return
+				}
+			}
+		} else {
+			fs.WalkDir(o.fsys, o.name.String(), func(pathname string, d fs.DirEntry, err error) error {
+				ok := yield(path{o.container, o.fsys, internpath.New(pathname)}, d.Type())
+				if !ok {
+					return io.EOF // any error is fine
+				}
+				return nil
+			})
+		}
+	}
 }
 
 // path turns a string into our internal path representation
